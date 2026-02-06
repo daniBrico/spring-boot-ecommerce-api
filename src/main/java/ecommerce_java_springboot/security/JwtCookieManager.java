c578e5f4d2b564777bdb455ce6ebdf2aa7e9067a
Daniel <daniel.jorge96@outlook.com>
2026-02-06 01:20:20 -0300

    Add jwt into cookie handler

---
+++ src/main/java/ecommerce_java_springboot/security/JwtCookieManager.java
@@ -0,0 +1,17 @@
+package ecommerce_java_springboot.security;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtCookieManager {
+
+  public void addJwtCookie(HttpServletResponse response, String token) {
+    Cookie cookie = new Cookie("jwt", token);
+    cookie.setHttpOnly(true);
+    cookie.setPath("/");
+    cookie.setMaxAge(60 * 60); // Debería ponerlo en una variable de entorno
+    response.addCookie(cookie);
+  }
+}
