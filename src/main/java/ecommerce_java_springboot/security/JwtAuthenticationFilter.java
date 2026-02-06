01acbc8c629406b63ea85d4a14ca0f8b27e88ca7
Daniel <daniel.jorge96@outlook.com>
2026-02-06 01:22:51 -0300

    Add NonNull annotation to parameters

--- src/main/java/ecommerce_java_springboot/security/JwtAuthenticationFilter.java
+++ src/main/java/ecommerce_java_springboot/security/JwtAuthenticationFilter.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import lombok.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -26,9 +27,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
   @Override
   protected void doFilterInternal(
-    HttpServletRequest request,
-    HttpServletResponse response,
-    FilterChain filterChain
+    @NonNull HttpServletRequest request,
+    @NonNull HttpServletResponse response,
+    @NonNull FilterChain filterChain
   ) throws ServletException, IOException {
 
     String token = extractTokenFromCookie(request);
