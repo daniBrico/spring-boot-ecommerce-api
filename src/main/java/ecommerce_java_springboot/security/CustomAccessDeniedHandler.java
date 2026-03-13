a048de375a718dae1bb07ec3184403c09a61961a
Daniel <daniel.jorge96@outlook.com>
2026-03-13 12:41:44 -0300

    Add exceptions for not authenticated and not authorized user

---
+++ src/main/java/ecommerce_java_springboot/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,36 @@
+package ecommerce_java_springboot.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AccessDeniedException accessDeniedException
+    ) throws IOException {
+
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setContentType("application/json");
+
+        String body = """
+        {
+            "status": 403,
+            "error": "Forbidden",
+            "message": "You do not have permission to access this resource",
+            "path": "%s"
+        }
+        """.formatted(request.getRequestURI());
+
+        response.getWriter().write(body);
+    }
+}
---
+++ src/main/java/ecommerce_java_springboot/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,36 @@
+package ecommerce_java_springboot.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException
+    ) throws IOException {
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType("application/json");
+
+        String body = """
+                {
+                    "status": 401,
+                    "error": "Unauthorized",
+                    "message": "Authentication required",
+                    "path": "%s"
+                }
+                """.formatted(request.getRequestURL());
+
+        response.getWriter().write(body);
+    }
+}
