c954be543f2d826289cf2572c50667a27016fdc3
Daniel <daniel.jorge96@outlook.com>
2026-03-17 22:43:33 -0300

    fix: add post mapping in addItem route

--- src/main/java/ecommerce_java_springboot/controllers/CartController.java
+++ src/main/java/ecommerce_java_springboot/controllers/CartController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class CartController {
     @Autowired
     private CartService cartService;
 
+    @PostMapping
     public ResponseEntity<Void> addItem(@RequestBody AddCartItemRequest request){
 
         cartService.addItem(request);
