68507cd5f77f232adb99379f349d937cd9ee893e
Daniel <daniel.jorge96@outlook.com>
2026-03-17 23:27:46 -0300

    refactor: delete unused variable productRepository

--- src/main/java/ecommerce_java_springboot/controllers/ProductController.java
+++ src/main/java/ecommerce_java_springboot/controllers/ProductController.java
@@ -17,8 +17,6 @@ import java.util.List;
 public class ProductController {
     @Autowired
     ProductService productService;
-    @Autowired
-    private ProductRepository productRepository;
 
     @GetMapping
     public ResponseEntity<List<ProductDTO>> getProducts() {
