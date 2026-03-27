afd6edfae9d40d8278515c25481472b530aacc0b
Daniel <daniel.jorge96@outlook.com>
2026-03-27 20:02:02 -0300

    feat: add method-level authorization using @PreAuthorize and enable method security

--- src/main/java/ecommerce_java_springboot/security/SecurityConfig.java
+++ src/main/java/ecommerce_java_springboot/security/SecurityConfig.java
@@ -3,14 +3,18 @@ package ecommerce_java_springboot.security;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @AllArgsConstructor
 public class SecurityConfig {
 
@@ -21,14 +25,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
 
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
                 .authorizeHttpRequests(request ->
                         request
                                 .requestMatchers("/auth/**").permitAll()
-                                .anyRequest().authenticated())
+                                .requestMatchers(HttpMethod.POST, "/products").hasRole("ADMIN")
+                                .anyRequest().authenticated()
+                )
 
                 .exceptionHandling(exception ->
                         exception
