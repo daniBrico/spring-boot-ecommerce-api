e97fca6beee866d841dbaa3e55ce128f0d71056f
Daniel <daniel.jorge96@outlook.com>
2026-04-01 18:49:40 -0300

    refactor: restrict cart creation and fetching to CUSTOMER role

--- src/main/java/ecommerce_java_springboot/controllers/CartController.java
+++ src/main/java/ecommerce_java_springboot/controllers/CartController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,12 +17,14 @@ public class CartController {
     @Autowired
     private CartService cartService;
 
+    @PreAuthorize("hasRole('CUSTOMER')")
     @PostMapping
     public ResponseEntity<CartResponse> addItem(@Valid @RequestBody AddCartItemRequest request){
         CartResponse cart = cartService.addItem(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(cart);
     }
 
+    @PreAuthorize("hasRole('CUSTOMER')")
     @GetMapping
     public ResponseEntity<CartResponse> getCart(){
         CartResponse cart = cartService.getCart();
